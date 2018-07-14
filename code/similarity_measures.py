fee52388d03b65e9e6d8011b882516f1ef5561b8
cjekel <cjekel@gmail.com>
2018-07-14 17:46:16 -0400

    add discrete frechet code

--- code/similarity_measures.py
+++ code/similarity_measures.py
@@ -322,5 +322,66 @@ def curve_length_measure(exp_data, num_data):
             np.log(1.0 + (np.abs(ytemp-y_e[i])/ymean))**2
     return np.sqrt(np.sum(r_sq))
 
+
+def euc_dist(pt1, pt2):
+    """
+    calcuates the Euclidean distance between two points
+    
+    Thanks to MaxBareiss
+    https://gist.github.com/MaxBareiss/ba2f9441d9455b56fbc9
+    """
+    return ((pt2[0]-pt1[0])*(pt2[0]-pt1[0])+(pt2[1]-pt1[1])*(pt2[1]-pt1[1]))**0.5
+
+def _c(ca, i, j, P, Q):
+    """
+    Recursive caller for discrete frechet distance as defined in [3]
+
+    Thanks to MaxBareiss
+    https://gist.github.com/MaxBareiss/ba2f9441d9455b56fbc9
+
+    References:
+    [3] Thomas Eiter and Heikki Mannila. Computing discrete Fréchet distance.
+    Technical report, 1994.
+    http://www.kr.tuwien.ac.at/staff/eiter/et-archive/cdtr9464.pdf
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.90.937&rep=rep1&type=pdf
+    """
+    if ca[i,j] > -1:
+        return ca[i,j]
+    elif i == 0 and j == 0:
+        ca[i,j] = euc_dist(P[0],Q[0])
+    elif i > 0 and j == 0:
+        ca[i,j] = max(_c(ca,i-1,0,P,Q),euc_dist(P[i],Q[0]))
+    elif i == 0 and j > 0:
+        ca[i,j] = max(_c(ca,0,j-1,P,Q),euc_dist(P[0],Q[j]))
+    elif i > 0 and j > 0:
+        ca[i,j] = max(min(_c(ca,i-1,j,P,Q),_c(ca,i-1,j-1,P,Q),_c(ca,i,j-1,P,Q)),euc_dist(P[i],Q[j]))
+    else:
+        ca[i,j] = float("inf")
+    return ca[i,j]
+
+
+
+def frechet_dist(P,Q):
+    """
+    Notes:
+    
+
+    Thanks to MaxBareiss
+    https://gist.github.com/MaxBareiss/ba2f9441d9455b56fbc9
+
+    References:
+    [3] Thomas Eiter and Heikki Mannila. Computing discrete Fréchet distance.
+    Technical report, 1994.
+    http://www.kr.tuwien.ac.at/staff/eiter/et-archive/cdtr9464.pdf
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.90.937&rep=rep1&type=pdf
+    """
+    # Computes the discrete frechet distance between two polygonal lines
+    # Algorithm: http://www.kr.tuwien.ac.at/staff/eiter/et-archive/cdtr9464.pdf
+    # P and Q are arrays of 2-element arrays (points)
+    ca = np.ones((len(P),len(Q)))
+    ca = np.multiply(ca,-1)
+    return _c(ca,len(P)-1,len(Q)-1,P,Q)
+
+
 # https://docs.python.org/3/library/sys.html#sys.setrecursionlimit
 # https://stackoverflow.com/questions/3323001/what-is-the-maximum-recursion-depth-in-python-and-how-to-increase-it
