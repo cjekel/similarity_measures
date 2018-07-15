42f7cd6166776252a4e2d989af5cc1976b039044
cjekel <cjekel@gmail.com>
2018-07-15 19:12:53 -0400

    remove code

--- code/frechet_distance/compile_cython.sh
+++
@@ -1,4 +0,0 @@
-cython cfrechet.pyx
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -o cfrechet.so cfrechet.c
-cython c_frechet.pyx
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -I/usr/lib64/python2.7/site-packages/numpy/core/include -o c_frechet.so c_frechet.c
