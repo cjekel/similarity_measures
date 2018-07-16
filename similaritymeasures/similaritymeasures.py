40422b4aba4523299ff6eb78cb04dd59c13e6757
cjekel <cjekel@gmail.com>
2018-07-15 20:19:20 -0400

    thanks to Mahdi for shoelace form

--- similaritymeasures/similaritymeasures.py
+++ similaritymeasures/similaritymeasures.py
@@ -40,6 +40,9 @@ def poly_area(x, y):
 
     Returns:
     (float) - Area of the polynomial via the shoelace formula
+
+    Thanks to Mahdi on
+    https://stackoverflow.com/questions/24467972/calculate-area-of-polygon-given-x-y-coordinates
     """
     return 0.5*np.abs(np.dot(x, np.roll(y, 1))-np.dot(y, np.roll(x, 1)))
 
