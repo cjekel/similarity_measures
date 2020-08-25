bfb913124fd4aa2421a1d0a024f57e86d771d408
Charles Jekel <cjekel@gmail.com>
2020-08-25 09:33:43 -0700

    remove setuptools as runtime dependency

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
-from setuptools import setup
+
+import io
+from distutils.core import setup
+
+# load the version from version.py
+version = {}
+with open("similaritymeasures/version.py") as fp:
+    exec(fp.read(), version)
+
 setup(
     name='similaritymeasures',
-    version=open('similaritymeasures/VERSION').read().strip(),
+    version=version["__version__"],
     author='Charles Jekel',
     author_email='cjekel@gmail.com',
     packages=['similaritymeasures'],
-    package_data={'similaritymeasures': ['VERSION']},
     url='https://github.com/cjekel/similarity_measures',
     license='MIT License',
     description='Quantify the difference between two arbitrary curves in space',  # noqa E501
     long_description=open('README.md', encoding="utf-8").read(),
-    long_description_content_type='text/markdown',
+    # long_description_content_type='text/markdown',
     platforms=['any'],
     install_requires=[
         "numpy >= 1.14.0",
         "scipy >= 0.19.0",
-        "setuptools >= 38.6.0",
     ],
 )
