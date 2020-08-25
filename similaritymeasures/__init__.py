fec51b2c9cf8f8ba47a29df19c2dd40cd2e33745
Charles Jekel <cjekel@gmail.com>
2020-08-25 09:39:59 -0700

    fix __init__.py to point to version.py

--- similaritymeasures/__init__.py
+++ similaritymeasures/__init__.py
@@ -1,6 +1,2 @@
 from .similaritymeasures import *  # noqa F403
-import os  # noqa F401
-
-# add rudimentary version tracking
-VERSION_FILE = os.path.join(os.path.dirname(__file__), 'VERSION')
-__version__ = open(VERSION_FILE).read().strip()
+from .version import __version__  # noqa F401
