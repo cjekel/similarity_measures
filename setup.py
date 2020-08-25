f9dcd8e1a73db84e18071ea5d7eb5026204d41b5
cjekel <cjekel@gmail.com>
2020-08-25 09:50:40 -0700

    switch setup to readme rst

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/cjekel/similarity_measures',
     license='MIT License',
     description='Quantify the difference between two arbitrary curves in space',  # noqa E501
-    long_description=io.open('README.md', encoding="utf-8").read(),
+    long_description=io.open('README.rst', encoding="utf-8").read(),
     # long_description_content_type='text/markdown',
     platforms=['any'],
     install_requires=[
