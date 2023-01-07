fdc26517b23759bb0b3a50407781c86572e12bad
Nuc94 <dan.dom.nuc@gmail.com>
2023-01-07 20:57:58 +0100

    removed path from tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,3 @@
-import sys
-# caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '../similaritymeasures')
-
 import numpy as np
 import unittest
 import similaritymeasures
